a1d9f941753fe247183f369e19ae7b70dc6bfda8
Uche Jude Nnodim <uchejudennodim@gmail.com>
2023-02-20 13:51:40 +0100

    Update Batch ETL data pipeline.py

--- Batch ETL data pipeline.py
+++ Batch ETL data pipeline.py
@@ -6,10 +6,10 @@ import sys
 from sqlalchemy import create_engine
 
 # Connect to MySQL
-database = 'sales'
-user = 'admin'
-password = 'admin123!'
-host = 'practice-database.clnqvq7wvxsr.us-east-1.rds.amazonaws.com'
+database = 'your database'
+user = 'your username'
+password = 'your password'
+host = 'your hostname'
 connection = mysql.connector.connect(user=user, password=password,host=host,database=database)
 print ("Connected to MySQL database:",database, "as user:",user, "on host:",host)
 
@@ -47,9 +47,9 @@ for row in cursor.fetchall():
 
 
 # Connect to DB2
-dsn_hostname = "1bbf73c5-d84a-4bb0-85b9-ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud"
-dsn_uid = "drk24122"
-dsn_pwd = "244Pkd4s8q21cDTp"
+dsn_hostname = "your hostname"
+dsn_uid = "your username"
+dsn_pwd = "your password"
 dsn_port = "32286"
 dsn_database = "BLUDB"
 dsn_driver = "{IBM DB2 ODBC DRIVER}"
